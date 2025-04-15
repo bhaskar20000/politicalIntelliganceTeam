7d573223d8a1f5e31ed79ab3da5baca2080732ad
bhaskar20000 <bhaskarjavvadi2020@gmail.com>
2025-04-15 11:30:34 +0530

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,23 @@
+import pit_image from "../../pit_image.jpg";
+import "./index.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="laptop">
+        <img className="logo-image" src={pit_image} alt="logo" />
+        <h1 className="nav-heading">POLITICAL INTELLIGENCE TEAM</h1>
+        <p className="nav-name">Pavan</p>
+      </div>
+      <div className="mobile-nav">
+        <img className="logo-image" src={pit_image} alt="logo" />
+        <div className="right-nav-container">
+          <h1 className="nav-heading">POLITICAL INTELLIGENCE TEAM</h1>
+          <p className="nav-name">Pavan</p>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
