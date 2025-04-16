e7a76df829f1d1d52c59c5fe4f850ff44227c03e
bhaskar20000 <bhaskarjavvadi2020@gmail.com>
2025-04-16 16:38:46 +0530

    16 commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "../src/components/Navbar";
+import Body from "../src/components/Body";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Body />
     </div>
   );
 };
---
+++ src/components/Body/index.js
@@ -0,0 +1,12 @@
+import "./index.css";
+
+const Body = () => {
+  return (
+    <div>
+      <p>Today's quote:</p>
+      <p>Demoncracy is the most powerfull weapon in the world - Pavan</p>
+    </div>
+  );
+};
+
+export default Body;
